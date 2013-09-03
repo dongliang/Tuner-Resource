4e2d70cbd69e11833038f7ee65e1f89c469ca84c
dongliang <dongliang17@126.com>
2013-09-03 11:59:02 +0800

    first commit.

---
+++ Unity/caches/assets/test/TestTTDB.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using TTDB;
+public class TestTTDB : MonoBehaviour {
+
+	// Use this for initialization 
+    void Start()
+    {
+        TDRoot.Instance.Init(Application.dataPath + "/Data/", new string[] { "MissionList","gun" });
+        TDRoot.Instance.AddDataTunner("MissionList", 210251);
+        TDRoot.Instance.AddDataTunner("gun", 16);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            GameObject.Destroy(GameObject.Find("Cube"));
+            Debug.Log("dis");
+        }
+        if (GameObject.Find("Cube") == null)
+        {
+            Debug.Log("yes");
+        }
+        else
+        { 
+            Debug.Log("no");
+        }
+	}
+}
